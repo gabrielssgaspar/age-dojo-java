bb365c021c2bda690de168b8888c5d8f431a6813
Gabriel Gaspar <gabrielssgaspar@gmail.com>
2018-02-06 23:23:09 -0200

    AuthorsNames, initial structure

---
+++ src/main/br/com/age/dojo/AuthorsNames.java
@@ -0,0 +1,42 @@
+package br.com.age.dojo;
+
+/*
+ * NOMES DE AUTORES DE OBRAS BIBLIOGRÁFICAS
+ * 
+ * http://dojopuzzles.com/problemas/exibe/nomes-de-autores-de-obras-bibliograficas/
+ * 
+ * Quando se lista o nome de autores de livros, artigos e outras publicações é comum que se apresente
+ * o nome do autor ou dos autores da seguinte forma: sobrenome do autor em letras maiúsculas,
+ * seguido de uma vírgula e da primeira parte do nome apenas com as iniciais maiúsculas.
+ * 
+ * Por exemplo:
+ * - SILVA, Joao
+ * - COELHO, Paulo
+ * - ARAUJO, Celso de
+ * - GUIMARAES
+ * - SILVA NETO, Joao
+ * - NETO, Joao
+ * 
+ * Seu desafio é fazer um programa que leia um número inteiro correspondendo ao número de nomes que
+ * será fornecido e, a seguir, leia estes nomes (que podem estar em qualquer tipo de letra) e
+ * imprima a versão formatada no estilo exemplificado acima.
+ * 
+ * As seguintes regras devem ser seguidas nesta formatação:
+ * 
+ * - o sobrenome será igual a última parte do nome e deve ser apresentado em letras maiúsculas;
+ * 
+ * - se houver apenas uma parte no nome, ela deve ser apresentada em letras maiúsculas (sem vírgula);
+ * 
+ * - se a última parte do nome for igual a "FILHO", "FILHA", "NETO", "NETA", "SOBRINHO", "SOBRINHA"
+ *   ou "JUNIOR" e houver duas ou mais partes antes, a penúltima parte fará parte do sobrenome;
+ *   
+ * - as partes do nome que não fazem parte do sobrenome devem ser impressas com a inicial maiúscula
+ *   e com as demais letras minúsculas;
+ *   
+ * - "da", "de", "do", "das", "dos" não fazem parte do sobrenome e não iniciam por letra maiúscula.
+ */
+public class AuthorsNames {
+	
+	
+
+}
---
+++ src/test/br/com/age/dojo/AuthorsNamesTest.java
@@ -0,0 +1,14 @@
+package br.com.age.dojo;
+
+import org.junit.BeforeClass;
+
+public class AuthorsNamesTest {
+	
+	private static AuthorsNames authorsNames;
+
+	@BeforeClass
+	public static void beforeClass() {
+		authorsNames = new AuthorsNames();
+	}
+
+}
